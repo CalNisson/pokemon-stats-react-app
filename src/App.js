e06a31e4522ba350749379d36b2e474d8aee33de
CalNisson <calnisson@gmail.com>
2023-06-26 18:17:39 -0400

    Add abilities and solve for some edge cases

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 
   const [pokemonName, setPokemonName] = useState("");
   const [pokemonChosen, setPokemonChosen] = useState(false);
-  const [pokemon, setPokemon] = useState({name: "", image: "", stats: [], types: []});
+  const [pokemon, setPokemon] = useState({name: "", image: "", stats: [], types: [], abilities: []});
   const handleKeyDown = (event) => {
     if (event.key === 'Enter') {
       event.preventDefault();
@@ -14,13 +14,15 @@ function App() {
     } 
   }
   const searchPokemon = () => {
-    Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then(
+    Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`).then(
       (response) => {
         setPokemon({
           name: capName(pokemonName),
           img: response.data.sprites.front_default,
           stats: response.data.stats,
-          types: response.data.types
+          types: response.data.types,
+          abilities: response.data.abilities,
+          dexNum: response.data.id
         });
         setPokemonChosen(true);
     }).catch((error) => {
@@ -30,10 +32,54 @@ function App() {
   }
 
   function capName(pokeName) {
-    const name = pokeName;
-    const firstLetter = name.charAt(0).toUpperCase();
-    const restName = name.slice(1);
-    return firstLetter + restName;
+    let name = pokeName;
+    let replDash = false;
+
+    if(name.indexOf("-mega") >= 0) {
+      name = name.split("-mega")[0];
+      name = "Mega " + name;
+    } else if(name.indexOf("-alola") >= 0) {
+      name = name.split("-alola")[0];
+      name = "Alolan " + name;
+    } else if(name.indexOf("-galar") >= 0) {
+      name = name.split("-galar")[0];
+      name = "Galarian " + name;
+    } else if(name.indexOf("-hisui") >= 0) {
+      name = name.split("-hisui")[0];
+      name = "Hisuian " + name;
+    } else if(name.indexOf("-null") >= 0) {
+      name = name.split("-null")[0];
+      name = "Type: Null";
+    } else if(name.indexOf("-z") >= 0 ) {
+      name = name.split("-z")[0];
+      name = name + "-Z";
+    } else if(name.indexOf("-mime") >= 0 ) {
+      name = name.split("-mime")[0];
+      name = name + ". Mime";
+    } else if(name.indexOf("-jr") >= 0 ) {
+      name = name.split("-jr")[0];
+      name = name + " Jr.";
+    } else if(name === "farfetchd") {
+      name = "Farfetch'd";
+    } else if(name.indexOf("-") >= 0) {
+      replDash = true;
+      name = name.replaceAll("-", " ");
+    }
+
+    const wordArr = name.split(" ");
+    let assembledName = "";
+    for(let i = 0; i < wordArr.length; i++) {
+      if(i > 0) {
+        assembledName = assembledName + " ";
+      }
+      const firstLetter = wordArr[i].charAt(0).toUpperCase();
+      const restName = wordArr[i].slice(1);
+      assembledName = assembledName + firstLetter + restName;
+    }
+    if(replDash === true) {
+      assembledName = assembledName.replaceAll(" ", "-");
+    }
+    return assembledName;
   }
 
   function genStatNames(poke_stats) {
@@ -43,6 +89,11 @@ function App() {
 
   function genStats(poke_stats) {
     const arrayItems = poke_stats.map((stat) => <h3>{stat.base_stat}</h3>);
+    return arrayItems;
+  }
+
+  function genAbilities(abilities) {
+    const arrayItems = abilities.map((ability) => <h4>{capName(ability.ability.name.replaceAll("-"," "))}</h4>);
     return arrayItems;
   }
 
@@ -67,12 +118,15 @@ function App() {
             <h1>Please choose a Pokemon</h1>
           ) : (
             <>
-              <h1>{pokemon.name}</h1>
+              <h1>#{pokemon.dexNum} - {capName(pokemon.name.toLowerCase())}</h1>
               <img className="Pokemon" src={pokemon.img} alt="Pokemon's sprite"/>
               <div className="Types">
                 <img className="Type" src={"https://www.serebii.net/pokedex-bw/type/" + pokemon.types[0].type.name + ".gif"} alt ="Pokemon's type"/>
                 {pokemon.types[1] && <img className="Type" src={"https://www.serebii.net/pokedex-bw/type/" + pokemon.types[1].type.name + ".gif"} alt ="Pokemon's Second type"/>}
               </div>
+              <h3>Abilities</h3>
+              <ul>{genAbilities(pokemon.abilities)}</ul>
+              <div></div>
               <div className="StatSection">
                 <div className="row">
                   <div className="column">
